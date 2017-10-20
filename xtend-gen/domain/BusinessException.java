2efb4abdbd086c618dfa7be0208fe055a6513358
fedepujol <federicodpujol@gmail.com>
2017-10-20 10:14:38 -0300

    Aplico .gitignore

--- xtend-gen/domain/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-public class BusinessException /* implements RuntimeException  */{
-  public BusinessException(final /* String */Object e) {
-    throw new Error("Unresolved compilation problems:"
-      + "\nThe method super(String) is undefined");
-  }
-}
